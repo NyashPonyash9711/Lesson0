ecb349c4af9d7428e18887aeb0270a5f849b5665
NyashPonyash9711 <ole199991@gmail.com>
2024-12-07 18:15:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+print ("Hello world!")
+print (type("Hello world!"))
+
+print (34)
+print(type (34))
+
+print (45.34)
+print (type(45.34))
+
+
---
+++ task1,2,3,4.py
@@ -0,0 +1,27 @@
+#1st program
+
+print((9**0.5)*5)
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+#Чтобы создать зависимость присвоил выражениям Переменные
+A=(2*2+2)
+B=(2*(2+2))
+
+print (A)
+print (B)
+print(A==B)
+
+#4th program
+
+a="123.456"
+print(a)
+b = float(a) * 10
+c = b // 1
+print(int(c)%10)
+
+
+
